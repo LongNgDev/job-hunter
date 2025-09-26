dc29672c3af1d491812c25f40f404f03c815c843
NightW0lfff <philong170101@gmail.com>
2025-09-27 01:48:21 +1000

    comp(sectionCard): Add Section Card that display statistic about the applications. using Card, Badge ui.

---
+++ client/job-hunter/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+import SectionCards from "@/components/section-card";
+import React from "react";
+
+function page() {
+	return <SectionCards />;
+}
+
+export default page;
---
+++ client/job-hunter/src/components/section-card.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react";
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./ui/card";
+import { Badge } from "./ui/badge";
+
+function SectionCards() {
+	return (
+		<div className="m-auto max-w-[100rem] min-w-sm">
+			<div className="grid grid-rows-4 gap-4 m-4 sm:grid-rows-2 sm:grid-cols-2 lg:grid-cols-4 ">
+				<Card className="flex flex-col gap-10">
+					<CardHeader>
+						<CardDescription>Total Applications</CardDescription>
+						<CardTitle className="text-4xl">20</CardTitle>
+						<CardAction>
+							<Badge variant="outline">
+								<IconTrendingUp />
+								+12.5%
+							</Badge>
+						</CardAction>
+					</CardHeader>
+
+					<CardFooter>
+						<p>Card Footer</p>
+					</CardFooter>
+				</Card>
+
+				{/* Application Submitted*/}
+				<Card className="flex flex-col gap-10">
+					<CardHeader>
+						<CardDescription>Submitted</CardDescription>
+						<CardTitle className="text-4xl">20</CardTitle>
+						<CardAction>
+							<Badge variant="outline">
+								<IconTrendingUp />
+								+12.5%
+							</Badge>
+						</CardAction>
+					</CardHeader>
+
+					<CardFooter>
+						<p>Card Footer</p>
+					</CardFooter>
+				</Card>
+
+				{/* In Progress */}
+				<Card className="flex flex-col gap-10">
+					<CardHeader>
+						<CardDescription>In Progress</CardDescription>
+						<CardTitle className="text-4xl">20</CardTitle>
+						<CardAction>
+							<Badge variant="outline">
+								<IconTrendingUp />
+								+12.5%
+							</Badge>
+						</CardAction>
+					</CardHeader>
+
+					<CardFooter>
+						<p>Card Footer</p>
+					</CardFooter>
+				</Card>
+
+				{/* Rejected */}
+				<Card className="flex flex-col gap-10">
+					<CardHeader>
+						<CardDescription>Reject</CardDescription>
+						<CardTitle className="text-4xl">20</CardTitle>
+						<CardAction>
+							<Badge variant="outline">
+								<IconTrendingUp />
+								+12.5%
+							</Badge>
+						</CardAction>
+					</CardHeader>
+
+					<CardFooter>
+						<p>Card Footer</p>
+					</CardFooter>
+				</Card>
+			</div>
+		</div>
+	);
+}
+
+export default SectionCards;
